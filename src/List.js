4056fb17f86fff2bd9505b748722a3ad89ecb774
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-03-17 18:48:25 +0530

    Add listI tems using props

--- src/List.js
+++ src/List.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const List = () => {
-  return (
-    <>
-      <h2>list component</h2>
-    </>
-  );
+const List = ({ people }) => (
+  <>
+    {
+      people.map((person) => {
+        const {
+          id, name, age, image,
+        } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>
+                {age}
+                {' '}
+                years
+              </p>
+            </div>
+
+          </article>
+        );
+      })
+    }
+  </>
+);
+
+List.propTypes = {
+  people: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      age: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
 };
 
 export default List;
